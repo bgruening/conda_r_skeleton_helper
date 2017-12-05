3c658d48d0401156a7f2c75c027183c0998de678
Björn Grüning <bjoern@gruenings.eu>
2017-12-05 17:04:39 +0100

    osx_compliant

--- run.sh
+++ run.sh
@@ -4,26 +4,26 @@ mkdir -p tmp
 for fn in `cat packages.txt`; do
     conda skeleton cran $fn
     cp $fn/meta.yaml tmp/$fn.meta.yaml
-    sed -i '/^\s*#.*$/ d' $fn/meta.yaml
-    sed -i '/^mv\s.*$/ d' $fn/build.sh
-    sed -i '/^grep\s.*$/ d' $fn/build.sh
-    sed -i '/^#\s.*$/ d' $fn/build.sh
-    sed -i '/^@.*$/ d' $fn/bld.bat
-    sed -i '/^$/{N;/^\n$/d;}' $fn/meta.yaml
-    sed -i '/^$/{N;/^\n$/d;}' $fn/build.sh
-    sed -i 's/ [+|] file LICEN[SC]E//' $fn/meta.yaml
-    sed  -i 's/{indent}/\n    - /' $fn/meta.yaml
+    sed -i.bak '/^\s*#.*$/ d' $fn/meta.yaml
+    sed -i.bak '/^mv\s.*$/ d' $fn/build.sh
+    sed -i.bak '/^grep\s.*$/ d' $fn/build.sh
+    sed -i.bak '/^#\s.*$/ d' $fn/build.sh
+    sed -i.bak '/^@.*$/ d' $fn/bld.bat
+    sed -i.bak '/^$/{N;/^\n$/d;}' $fn/meta.yaml
+    sed -i.bak '/^$/{N;/^\n$/d;}' $fn/build.sh
+    sed -i.bak 's/ [+|] file LICEN[SC]E//' $fn/meta.yaml
+    sed  -i.bak 's/{indent}/\n    - /' $fn/meta.yaml
     # skip win builds
-    sed -i 's/number: 0/number: 0\n  skip: true  # [win32]/g' $fn/meta.yaml
+    sed -i.bak 's/number: 0/number: 0\n  skip: true  # [win32]/g' $fn/meta.yaml
 
     # Add GPL-3
-    sed -i s/"  license_family: GPL3"/"  license_family: GPL3\n  license_file: '{{ environ[\"PREFIX\"] }}\/lib\/R\/share\/licenses\/GPL-3'  # [unix]\n  license_file: '{{ environ[\"PREFIX\"] }}\\\R\\\share\\\licenses\\\GPL-3'  # [win]"/ $fn/meta.yaml
+    sed -i.bak s/"  license_family: GPL3"/"  license_family: GPL3\n  license_file: '{{ environ[\"PREFIX\"] }}\/lib\/R\/share\/licenses\/GPL-3'  # [unix]\n  license_file: '{{ environ[\"PREFIX\"] }}\\\R\\\share\\\licenses\\\GPL-3'  # [win]"/ $fn/meta.yaml
     # Add GPL-2
-    sed -i s/"  license_family: GPL2"/"  license_family: GPL2\n  license_file: '{{ environ[\"PREFIX\"] }}\/lib\/R\/share\/licenses\/GPL-2'  # [unix]\n  license_file: '{{ environ[\"PREFIX\"] }}\\\R\\\share\\\licenses\\\GPL-2'  # [win]"/ $fn/meta.yaml
+    sed -i.bak s/"  license_family: GPL2"/"  license_family: GPL2\n  license_file: '{{ environ[\"PREFIX\"] }}\/lib\/R\/share\/licenses\/GPL-2'  # [unix]\n  license_file: '{{ environ[\"PREFIX\"] }}\\\R\\\share\\\licenses\\\GPL-2'  # [win]"/ $fn/meta.yaml
 
-    sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' $fn/bld.bat
-    sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' $fn/meta.yaml
-    sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' $fn/build.sh
+    sed -i.bak -e ':a' -e '/^\n*$/{$d;N;};/\n$/ba' $fn/bld.bat
+    sed -i.bak -e ':a' -e '/^\n*$/{$d;N;};/\n$/ba' $fn/meta.yaml
+    sed -i.bak -e ':a' -e '/^\n*$/{$d;N;};/\n$/ba' $fn/build.sh
     cat extra.yaml >> $fn/meta.yaml
     gedit $fn/meta.yaml
     diff -u $fn/meta.yaml tmp/$fn.meta.yaml > tmp/$fn.meta.diff
