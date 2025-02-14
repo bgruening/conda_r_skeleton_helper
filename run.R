bfe424ca92d9fcd1330f1dc075f70d4306e4996e
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-02-14 14:38:29 +0100

    include dash

--- run.R
+++ run.R
@@ -86,7 +86,7 @@ for (fn in packages) {
   meta_new <- str_replace(meta_new, fixed("{{native}}"), "")
   meta_new <- str_replace(meta_new, fixed("{{ posix }}pkg-config"), "pkg-config")
   meta_new <- str_replace(meta_new, fixed("{{posix}}pkg-config"), "pkg-config")
-  meta_new <- str_replace(meta_new, fixed("- m2w64-pkg-config"), "pkg-config")
+  meta_new <- str_replace(meta_new, fixed("- m2w64-pkg-config"), "- pkg-config")
   meta_new <- str_replace(meta_new, fixed("- m2w64-toolchain"), "- {{ compiler('m2w64_c') }}")
   meta_new <- str_replace(meta_new, fixed("- posix"), "- m2-base")
   meta_new <- meta_new[!str_detect(meta_new, fixed("merge_build_host: "))]
